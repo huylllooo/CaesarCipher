f0f59bafbe2019d975e8022218baf7485cbd5a19
Huy M. Nguyen <nmhuy14@gmail.com>
2016-04-03 15:35:24 +0900

    Added method encryptTwoKets enables 2 keys encrypting

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,12 +1,11 @@
 import edu.duke.*;
 
 public class CaesarCipher {
+        private String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private String alphabetLowCase = alphabet.toLowerCase();
     public String encrypt(String input, int key) {
         //Make a StringBuilder with message (encrypted)
         StringBuilder encrypted = new StringBuilder(input);
-        //Write down the alphabet
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String alphabetLowCase = alphabet.toLowerCase();
         //Compute the shifted alphabet
         String shiftedAlphabet = alphabet.substring(key) + alphabet.substring(0,key);
         String shiftedLowCase = alphabetLowCase.substring(key) + alphabetLowCase.substring(0,key);
@@ -35,6 +34,35 @@ public class CaesarCipher {
             //Otherwise: do nothing
         }
         //Your answer is the String inside of encrypted
+        return encrypted.toString();
+    }
+    private char encryptChar(char input, int key) {
+        char encrypted;
+        if (Character.isLowerCase(input)) {
+           int idx = alphabetLowCase.indexOf(input);
+           if (idx + key < 26)
+             encrypted = alphabetLowCase.charAt(idx + key);
+           else 
+             encrypted = alphabetLowCase.charAt(idx + key - 26);
+        } else {
+            int idx = alphabet.indexOf(input);
+           if (idx + key < 26)
+             encrypted = alphabet.charAt(idx + key);
+           else 
+             encrypted = alphabet.charAt(idx + key - 26);
+        }
+        return encrypted;
+    }
+    public String encrypt(String input, int key1, int key2) {
+        //Make a StringBuilder with message (encrypted)
+        StringBuilder encrypted = new StringBuilder(input);
+        for (int i = 0; i < encrypted.length(); i++) {
+            char currChar = encrypted.charAt(i);
+            if (i%2 == 0)
+                encrypted.setCharAt(i, encryptChar(currChar, key1));
+            else 
+                encrypted.setCharAt(i, encryptChar(currChar, key2));
+        }
         return encrypted.toString();
     }
     public void testCaesar() {
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Encrypt and decrypt messages using Ceasar Cipher with a key (default 17)
+
+###v1
+Be able to hangle both uppercase and lowercase letters
+###v2
+Can encrypt message using two keys by method encryptTwoKeys
